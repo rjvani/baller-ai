ee4c7d2cc87081fd4ba6af0f449aadb62a099a63
Kevin Zheng <sunday0303@gmail.com>
2016-05-14 00:42:16 -0400

    Mostly-working for 0-10

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import pyscreenshot
 from PIL import Image
 import time
 import numpy
+from autopy import mouse
 
 BBOX = (0,0,450,750)
-TARGET_COLOR = (255,38,15)
-TARGET_ROW = 260
+HOOP_COLOR = (255,38,15)
+HOOP_ROW = 260
 
 BALL_COLOR = (255,150,47)
 BALL_ROW = 670
+#BALL_ROW = 660
 
 def main():
   start = time.time()
@@ -18,25 +20,42 @@ def main():
     im = numpy.asarray(pyscreenshot.grab(bbox=BBOX))
 
     # find the hoop location
+    hoopcenter = None
     try:
-      row = map(tuple, list(im[TARGET_ROW]))
+      row = map(tuple, list(im[HOOP_ROW]))
       hoopcenter = get_hoop(row)
     except:
       print 'could not find hoop'
-      continue
 
     print hoopcenter
 
     # find the ball location
+    ballcenter = None
     try:
       row = map(tuple, list(im[BALL_ROW]))
       ballcenter = get_ball(row)
     except:
       print 'could not find ball'
-      continue
     
     print ballcenter
-    print
+
+    # drag the mouse
+    if ballcenter is not None:
+      x1 = hoopcenter
+      y1 = HOOP_ROW
+      x2 = ballcenter-9
+      y2 = BALL_ROW
+
+      mouse.move(x2, y2)
+      mouse.toggle(True, 1)
+      #time.sleep(0.1)
+      for y3 in range(y2, y1, -1):
+        x3 = int(float(y3-y1)/(y2-y1)*(x2-x1)+x1)
+        mouse.move(x3, y3)
+        time.sleep(0.0001)
+      mouse.toggle(False, 1)
+      time.sleep(2)
+
 
 
 def get_ball(row):
@@ -46,8 +65,8 @@ def get_ball(row):
   return ballcenter
 
 def get_hoop(row):
-  left = row.index(TARGET_COLOR)
-  right = len(row) - list(reversed(row)).index(TARGET_COLOR)
+  left = row.index(HOOP_COLOR)
+  right = len(row) - list(reversed(row)).index(HOOP_COLOR)
   hoopcenter = (left + right) // 2
   return hoopcenter
 
